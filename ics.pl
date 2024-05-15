c32be81074fe1e84fd33198021b5b0429e763774
Saif Ahmed <34284663+saiftynet@users.noreply.github.com>
2024-05-15 20:38:26 +0100

    Update ics.pl

--- ics.pl
+++ ics.pl
@@ -1,108 +1,91 @@
 #!/usr/bin/env perl
-#
+
 use strict;use warnings;
 use Data::Dumper;
-use DateTime;
-use utf8;
-
-binmode(STDOUT,"encoding(cp-1252)");
-my $calendar={events=>[],};
-my @levels;
-my $item;
-while  (my $line=<DATA>){
-   if ($line=~/^ /){
-      $item->{value}.=$line;
-	  next;
-   }
-   if ($line=~/BEGIN:V(.*)$/){
-     push @levels,$1;
-	 next;
-   }
-   elsif ($line=~/END:V(.*)$/){
-   
-     if ($levels[-1]=~/EVENT/){
-	    push @{$calendar->{events}},{%$item} if %$item;
-		pop @levels;
-		$item={};
-	 }
-	 else{
-		pop @levels
-	 }
-	 next;
-   }
-   my $modifiers=""; my $props={};
-   if ($line=~/^[^\;\:]+(\;[^\:]+)\:/){
-     $modifiers=$1;
-	 substr $modifiers,0,1,"";
-	 my @mods=split(";",$modifiers);
-	 foreach (@mods){
-	    my ($key,$value)=split ("=", $_);
-		$props->{$key}=$value if $key;
-	 }
-     $line=~s/$1//;
-   }
-   if ($levels[-1]=~/EVENT/){
-		if ($line=~/^(DTSTART|DTEND|SUMMARY|CATEGORIES|CLASS|DESCRIPTION|LOCATION)\:(.*)$/){
-	    $item->{lc $1}->{value}=$2;
-		$item->{lc $1}->{properties}=$props if $modifiers;
+my $calendar=loadICS();
+sub loadICS{
+	my $icsFile=shift;
+	our $calendar={events=>[],};
+	our @levels;
+	my $lastLine="";
+	our $item={};
+	while  (my $line=<DATA>){
+	   chomp $line;
+	   if($lastLine eq ""){
+		   $lastLine = $line;
 	   }
-   }
-   if ($levels[-1]=~/ALARM/){
-       $item->{alarm}//={};
-	   if ($line=~/^(TRIGGER|DESCRIPTION|ACTION)\:(.*)$/){
-	     $item->{alarm}->{lc $1}=$2;
-	   }
-   }
-   elsif ($line=~/X-WR-CALNAME\:(.*)$/){
-         $calendar->{name}=$1;
-   }
+	   elsif ($line=~/^\s/){
+		   $lastLine.=$line;
+		   parseLine($lastLine);
+		}
+		else{
+			parseLine($lastLine);
+			$lastLine=$line;
+		}
+	}
+	print $lastLine;	   
+	sub parseLine{
+		my $line=shift;
+		 if ($line=~/^BEGIN:V(.*)$/){
+			 push @levels,$1;
+			 return;
+		 }
+		 elsif ($line=~/^END:V(.*)$/){                          # end of section 
+			 if ($levels[-1]=~/EVENT/){
+				push @{$calendar->{events}},{%$item} if %$item;
+				$item={};
+			}
+			pop @levels;
+		}
+		elsif ($levels[-1]=~/EVENT/){
+			my @parts=($line=~/^([A-Z\-]+)(\;[^\;\:]+)?(\:.*)$/);
+			$item->{$parts[0]}=substr $parts[-1],1 if $parts[0];;
+		}
+		elsif ($levels[-1]=~/CALENDAR/){
+			my @parts=($line=~/^([A-Z\-]+)(\;[^\;]+)*(\:.*)$/);
+			$calendar->{$parts[0]}=substr $parts[-1],1 if $parts[0];;
+		}
+		else{
+			$item->{$levels[-1]}//={};
+			my @parts=($line=~/^([A-Z\-]+)(\;[^\;]+)*(\:.*)$/);
+			$item->{$levels[-1]}->{$parts[0]}=substr $parts[-1],1 if $parts[0];;
+		}
+	}
+	return $calendar
 }
-
-my @sortedEvents=sort{$a->{dtstart}->{value} <=>$b->{dtstart}->{value}} @{$calendar->{events}}; 
-
 print Dumper $calendar;
 
 __DATA__
 BEGIN:VCALENDAR
-VERSION:2.0
 PRODID:-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN
+VERSION:2.0
+BEGIN:VTIMEZONE
+TZID:Europe/Berlin
+X-LIC-LOCATION:Europe/Berlin
+BEGIN:DAYLIGHT
+TZOFFSETFROM:+0100
+TZOFFSETTO:+0200
+TZNAME:CEST
+DTSTART:19700329T020000
+RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=3
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:+0200
+TZOFFSETTO:+0100
+TZNAME:CET
+DTSTART:19701025T030000
+RRULE:FREQ=YEARLY;BYDAY=-1SU;BYMONTH=10
+END:STANDARD
+END:VTIMEZONE
 BEGIN:VEVENT
-SUMMARY:Dita e Vitit të Ri 
-DTSTART;VALUE=DATE:20240101
-DTEND;VALUE=DATE:20240102
-DTSTAMP:20240401T090003Z
-UID:albania/new-year-day-2024
-CATEGORIES:Holidays
-CLASS:public
-DESCRIPTION:National holiday -  Dita e Vitit të Ri është dita e parë e
-  vitit\, ose 1 janar\, në kalendarin gregorian.
-LAST-MODIFIED:20240401T090003Z
-TRANSP:transparent
-END:VEVENT
-BEGIN:VEVENT
-SUMMARY:Dita e Vitit të Ri (Dita 2) 
-DTSTART;VALUE=DATE:20240102
-DTEND;VALUE=DATE:20240103
-DTSTAMP:20240401T090003Z
-UID:albania/second-day-new-year-2024
-CATEGORIES:Holidays
-CLASS:public
-DESCRIPTION:National holiday -  Dita e Vitit të Ri (Dita 2) është një 
- festë kombëtare në Shqipëri
-LAST-MODIFIED:20240401T090003Z
-TRANSP:transparent
-END:VEVENT
-BEGIN:VEVENT
-SUMMARY:Dita e verës 
-DTSTART;VALUE=DATE:20240314
-DTEND;VALUE=DATE:20240315
-DTSTAMP:20240401T090003Z
-UID:albania/summer-day-2024
-CATEGORIES:Holidays
-CLASS:public
-DESCRIPTION:National holiday -  Dita e verës është një festë kombëta
- re në Shqipëri
-LAST-MODIFIED:20240401T090003Z
-TRANSP:transparent
+CREATED:20140107T092011Z
+LAST-MODIFIED:20140107T121503Z
+DTSTAMP:20140107T121503Z
+UID:20f78720-d755-4de7-92e5-e41af487e4db
+SUMMARY:Just a Test
+DTSTART;TZID=Europe/Berlin:20140102T110000
+DTEND;TZID=Europe/Berlin:20140102T120000
+X-MOZ-GENERATION:4
+DESCRIPTION:Here is a new Class:
 END:VEVENT
 END:VCALENDAR
